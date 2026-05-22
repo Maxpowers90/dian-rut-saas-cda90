d77e50a8468cb4e0e736ad0a13f870d5d93c0222
Maxpowers90 <calabozodelandroide90@gmail.com>
2026-05-21 23:17:36 -0500

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -3,6 +3,12 @@ import random
 import logging
 import traceback
 
+from playwright.async_api import (
+    async_playwright,
+    Error as PlaywrightError,
+    TimeoutError as PlaywrightTimeoutError,
+)
+
 logger = logging.getLogger("dian_scraper")
 
 USER_AGENTS = [
